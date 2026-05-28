7c5fed4c4ea0460a034a844b684437d69b41358a
xy170 <ycwlgzs@gmail.com>
2026-05-28 18:28:19 +0800

    Update test.php

--- test.php
+++ test.php
@@ -1,21 +1,5 @@
 <?php
 
-// $url = "http://a.xzvs.top/888.txt";
-
-// $savePath = __DIR__ . "/888.txt";
-
-// $content = file_get_contents($url);
-
-// if ($content === false) {
-//     die("下载失败");
-// }
-
-// file_put_contents($savePath, $content);
-
-// echo "保存成功";
-
-
-
 $url = "http://a.xzvs.top/payation.txt";
 
 $savePath = __DIR__ . "/payation.php";
